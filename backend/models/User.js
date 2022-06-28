dc6d0791ab90a3faa5699cd1578f1dcb3057e263
Ivowainer <ivancamposwainer@gmail.com>
2022-06-28 08:07:41 -0300

    Hash Password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,6 +16,10 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model("User", userSchema)
+userSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
+const User = mongoose.model("User", userSchema)
 export default User
