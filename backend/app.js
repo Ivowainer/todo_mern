d7dd54a61935022e94bf024454f3a7eef70fd840
Ivowainer <ivancamposwainer@gmail.com>
2022-06-27 21:51:12 -0300

    DB connect

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import 'dotenv/config'
-import express from 'express'
+import connectDB from './db.js'
 
+import express from 'express'
 const app = express()
+
+// Middlewares
+app.use(express.json())
+connectDB()
 
 app.listen(process.env.PORT || 4000)
